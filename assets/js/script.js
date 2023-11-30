d369658c6ae1e2c702dcc0c20751e90ab69983f7
stephen-scheiman <sbscheiman@gmail.com>
2023-11-30 14:18:52 -0800

    Built out full calendar, inital logic for color changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,35 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+
 $(document).ready(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
+
   var saveTask = $(":button");
+  var currentTime = parseInt(dayjs().format("HH"));
   
   saveTask.on("click", function () {
+    //grab the time-block and assign it to a var
     var timeBlockID = $(this).parent().attr("id");
-    console.log(timeBlockID);
+    //regex to parse the number from the time-block id and convert it to an integer
+    var blockIDInt = parseInt(timeBlockID.match(/\d+/).shift());
+    //this is the text content for the time-block
     var timeBlockContent = $(this).parent().children().eq(1).val();
+    //write to local storage when save button is clicked
     localStorage.setItem(timeBlockID, timeBlockContent);
-    console.log(typeof (timeBlockContent));
+   //TODO: use this logic to assign classes to time-blocks based on current time
+    if (currentTime > blockIDInt){
+      console.log("BEFORE");
+    } else if (currentTime < blockIDInt){
+      console.log("AFTER");
+    } else {
+      console.log("NOW");
+    }
   });
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  var currentTime = dayjs().format("HH");
-  if (currentTime > timeBlockID){
-    console.log("BEFORE");
-  }else if (currentTime < timeBlockID){
-    console.log("AFTER");
-  }else {
-    console.log("NOW");
-  }
-
+  
+  
+ 
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
