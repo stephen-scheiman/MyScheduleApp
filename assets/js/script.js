4aa38787e30dc1c5dd46e1d5ad18f82502205a5b
stephen-scheiman <sbscheiman@gmail.com>
2023-12-02 08:09:48 -0800

    Fully functioning MVP; all acceptance crit met

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,53 @@
 $(document).ready(function () {
-  var saveTask = $(':button');
-  var currentTime = parseInt(dayjs().format('HH'));
-  var arr = [];
+  var saveTask = $(":button");
+  var tmp = "";
+  var dateTime = $("#currentDay");
+  var currentTime = dayjs().format("HH");
 
-  saveTask.on('click', function () {
+  saveTask.on("click", function () {
     //grab the time-block and assign it to a var
     var timeBlockID = $(this).parent().attr("id");
-    //regex to parse the number from the time-block id and convert it to an integer
-    var blockIDInt = parseInt(timeBlockID.match(/\d+/).shift());
     //this is the text content for the time-block
     var timeBlockContent = $(this).parent().children().eq(1).val();
     //write to local storage when save button is clicked
     localStorage.setItem(timeBlockID, timeBlockContent);
-    //TODO: use this logic to assign classes to time-blocks based on current time
-      if (currentTime > blockIDInt) {
-        console.log('BEFORE');
-      } else if (currentTime < blockIDInt) {
-        console.log('AFTER');
-      } else {
-        console.log('NOW');
-      }
   });
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
 
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  var tmp = "";
-  var tmpLocation = "";
-  for (i=9; i<=17; i++){
-  tmp = localStorage.getItem('hour-'+[i]);
-  console.log($('#hour-'+[i]).children().eq(1).val());
+  changeClassbyTime();
+
+  //This function compares the current time to the integer value of a time block and adds
+  //the appropriate color class
+  function changeClassbyTime() {
+    for (i = 9; i <= 17; i++) {
+      tmp = $("#hour-" + [i]);
+      var tmpInt = parseInt(tmp.attr("id").match(/\d+/).shift());
+      if (tmpInt < currentTime){
+        tmp.addClass("past");
+      } else if (tmpInt > currentTime){
+        tmp.addClass("future");
+      } else {
+        tmp.addClass("now");
+      }
+    }
+  }
+
+  //This retrieves any items saved to local storage and renders them to the appropriate time-block
+  for (i = 9; i <= 17; i++) {
+    tmp = localStorage.getItem("hour-" + [i]);
+    if (
+      $("#hour-" + [i])
+        .children()
+        .eq(1)
+        .val() !== "null"
+    ) {
+      $("#hour-" + [i])
+        .children()
+        .eq(1)
+        .val(tmp);
+    }
   }
 
   // Use dayjs to get current time, format it, and display it in the header of the page
-  var dateTime = $('#currentDay');
   setInterval(function () {
     var currentTime = dayjs().format("MM/DD/YYYY HH:MM:ss");
     displayDate(currentTime);
@@ -48,3 +57,5 @@ $(document).ready(function () {
     dateTime.text(currentTime);
   }
 });
+
+
